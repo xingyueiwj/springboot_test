ca82f774fcba1bf7c0b7764e0b726aa6b313bc45
yangwanbiao <ywb10.8@qq.com>
2017-10-25 18:44:16 +0800

    springboot-test

--- src/main/resources/static/webpage/controller/owner/js/owner.js
+++ src/main/resources/static/webpage/controller/owner/js/owner.js
@@ -132,7 +132,41 @@ function addMyArticle() {
     var articleTitle = $("#myArticleTitle").val();
     var articleCotent = $("#myArticleDetail").val();
     if(articleTitle && articleCotent){
-        debugger;
+            $('#send').click(function(){
+
+                $.ajax({
+
+                    type: "GET",
+
+                    url: "test.json",
+
+                    data: {username:$("#username").val(), content:$("#content").val()},
+
+                    dataType: "json",
+
+                    success: function(data){
+
+                        $('#resText').empty(); //清空resText里面的所有内容
+
+                        var html = '';
+
+                        $.each(data, function(commentIndex, comment){
+
+                            html += '<div class="comment"><h6>' + comment['username']
+
+                                + ':</h6><p class="para"' + comment['content']
+
+                                + '</p></div>';
+
+                        });
+
+                        $('#resText').html(html);
+
+                    }
+
+                });
+
+            });
     }
 }
 
